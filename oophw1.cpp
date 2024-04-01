53ace08c96fbf21ba681c773c87753c2ddcc8c57
uaena <112924804+uaena030@users.noreply.github.com>
2024-04-02 00:45:21 +0800

    4/2
    input complete

--- oophw1.cpp
+++ oophw1.cpp
@@ -5,7 +5,9 @@ using namespace std;
 
 
 class PQB{
-    int ID, LQBID, nPQBIDs;
+    public:
+    int ID, LQBID;
+    vector<int> pneighbor;
 };
 
 class LQB{
@@ -13,20 +15,33 @@ class LQB{
 };
 
 class gate{
-    int precedence, LQBIDs;
+    public:
+    vector<int> precedence;
+    int gatemember[2];
 };
 
 int main(){
-    vector<int> i;
-    i.resize(5);
-    i[0] = 1;
-    i[1] = 2;
-    i[2] = 3;
-    i[3] = 4;
-    i[4] = 5;
-    cin >> i[0];
-    for(int j = 0; j < i.size(); j++){
-        cout << i[j] << " ";
+    vector<LQB> myLQB;
+    vector<gate> mygate;
+    vector<PQB> myPQB;
+    int logQubits, gates, precedence, phyQubits, phyLinks;
+    cin >> logQubits >> gates >> precedence >> phyQubits >> phyLinks;
+    myLQB.resize(logQubits + 1);
+    mygate.resize(gates + 1);
+    myPQB.resize(phyQubits + 1);
+    for(int i = 1; i <= gates; i++){//gate input
+        int trash;
+        cin >> trash >> mygate[i].gatemember[0] >> mygate[i].gatemember[1];
     }
-    cout << endl;
+    for(int i = 1; i <= precedence; i++){//precedence input
+        int current, next, trash;
+        cin >> trash >> current >> next;
+        mygate[current].precedence.push_back(next); 
+    }
+    for(int i = 1; i <= phyLinks; i++){
+        int p1, p2, trash;
+        cin >> trash >> p1 >> p2;
+        myPQB[p1].pneighbor.push_back(p2);
+        myPQB[p2].pneighbor.push_back(p1);
+    }
 }
