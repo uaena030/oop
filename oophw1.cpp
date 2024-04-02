2fe5dd97f92f5e97a01736743718ac0bd50375d8
uaena030 <112924804+uaena030@users.noreply.github.com>
2024-04-02 16:26:44 +0800

    4/2
    new

--- oophw1.cpp
+++ oophw1.cpp
@@ -6,12 +6,12 @@ using namespace std;
 
 class PQB{
     public:
-    int ID, LQBID;
+    int LQBID;
     vector<int> pneighbor;
 };
 
 class LQB{
-    int ID, PQBID;
+    int PQBID;
 };
 
 class gate{
@@ -29,7 +29,7 @@ int main(){
     myLQB.resize(logQubits + 1);
     mygate.resize(gates + 1);
     myPQB.resize(phyQubits + 1);
-    for(int i = 1; i <= gates; i++){//gate input
+    for(int i = 1; i <= gates; i++){//gate input(may not exist in phyical links)
         int trash;
         cin >> trash >> mygate[i].gatemember[0] >> mygate[i].gatemember[1];
     }
@@ -38,10 +38,15 @@ int main(){
         cin >> trash >> current >> next;
         mygate[current].precedence.push_back(next); 
     }
-    for(int i = 1; i <= phyLinks; i++){
+    for(int i = 1; i <= phyLinks; i++){//physical links input
         int p1, p2, trash;
         cin >> trash >> p1 >> p2;
         myPQB[p1].pneighbor.push_back(p2);
         myPQB[p2].pneighbor.push_back(p1);
     }
+    //baseline assume that logqubits equal to phyqubits
+    for(int i = 1; i <= myPQB.size(); i++){
+        myPQB[i].LQBID = i;
+    }
+    
 }
