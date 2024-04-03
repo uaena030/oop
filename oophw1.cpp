2d5b36054850fa9f838b548a6b455697aea31518
uaena030 <112924804+uaena030@users.noreply.github.com>
2024-04-03 17:16:47 +0800

    4/3
    trash day ever

--- oophw1.cpp
+++ oophw1.cpp
@@ -38,8 +38,6 @@ int main(){
         int current, next, trash;
         cin >> trash >> current >> next;
         mygate[current].precedence.push_back(next);
-        map<vector<gate>, vector<gate>> precedencelist;
-        precedencelist.insert(pair<vector<gate>, vector<gate>(mygate[current], mygate[next]));
     }
     for(int i = 1; i <= phyLinks; i++){//physical links input
         int p1, p2, trash;
